71304876ee3f38e1ee944cccb1f0d91cf5050d69
chrischau94@gmail.com <chrischau94@gmail.com>
2019-09-24 18:05:41 -0600

    Initial Commit

---
+++ DisappearingUnicorns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisappearingUnicorns
+//
+//  Created by Chris Chau on 2019-09-24.
+//  Copyright © 2019 Chris Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
